d06de2fb3325e1280496b7c104b4279d5343d79a
Steve Kavuu <mrkavuu@gmail.com>
2020-10-15 21:28:06 -0500

    added to generate markdown

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-var inquirer = require('inquirer');
+/* var inquirer = require('inquirer');
+var fs = require('fs')
+const generateMarkdown = require("./utils/generateMarkdown") */
+
+const generateMarkdown = require("./utils/generateMarkdown");
+var inquirer = require("inquirer")
+
+var fs = require("fs");
 
 
-// array of questions for user
+// array of questions for user 
 const questions = [
-    inquirer.prompt([
         {
             name: 'userName',
             type: 'input',
             message:'What is your GitHub username?'
         },
         {
-            name: 'email address',
+            name: 'email',
             type: 'input',
             message:'What is your email address?'
         },
@@ -24,11 +30,18 @@ const questions = [
             type: 'input',
             message:'Please enter the Project description?'
         },
-        {
+        /* {
+            
             name: 'projectLicense',
-            type: 'input',
+            type: 'list',
             message:'What kind of licence should your project have?'
-        },
+            choices: [
+                "MIT",
+                "Apache 2.0",
+                "GNU GPL v3.0",
+                "none"
+            ]
+        }, */
         {
             name: 'commandDependecies',
             type: 'input',
@@ -44,18 +57,43 @@ const questions = [
             type: 'input',
             message:'What does the user need to know about using the repo?'
         },
-       
-])
+    ];
 
-const generateMarkdown = require('u')
 
-// function to write README file
-function writeToFile(fileName, data) {
+
+// function used to determine what license was chosen and will later be used in init function to display badge image through generateMarkdown.
+const getLicense = (license) => {
+    
+    if(license === "MIT") {
+        return `\r[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+    }
+    else if (license === "Apache 2.0") {
+        return `\r[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+    }
+    else if (license === "GNU GPL v3.0") {
+        return `\r[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+        
+    }
 }
+
 
 // function to initialize program
 function init() {
+    // inquirer prompt will cause the questions to appear for user
+    inquirer.prompt(questions).then(function(data){
+       
+    
+        
+    
+    // writes file 'README.md'
+    fs.writeFile("READMEE.md", generateMarkdown(data), function(error) {
 
+        if(error) {
+            return console.log(error);
+        }
+        console.log("New README file created!");
+        })
+    })
 }
 
 // function call to initialize program
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,33 @@
+
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ${badge}
+  
+  ##Description
+  ${data.description}
+  ## Table of Contents
+  [Description](#Description)
+  [Installation](#Installation)
+  [Usage](#Usage)
+  [License](#License)
+  [Contributions](#Contributions)
+  [Test](#Test)
+  [Questions](#Questions)
+  ##Installation
+  
+  ${data.installation}
+  ##Usage
+  ${data.usage}
+  ##License
+  ${about}
+  ##Contributions
+  ${data.contributions}
+  ##Test
+  ${data.test}
+  ##Questions
+  github.com/${data.gitHubUser}
+  If there are any questions about this project you can reach me at ${data.email} and I will get back to you as soon as possible.
 `;
 }
 
